f8306d599546546fa57d2fd490396c8f5cc650ce
yanghan-a <12112522@mail.sustech.edu.cn>
2024-11-13 16:56:02 +0800

    final_lab4

--- ros_workspace/src/lab4/scripts/process_and_publish.py
+++ ros_workspace/src/lab4/scripts/process_and_publish.py
@@ -6,7 +6,7 @@ import itertools
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
 
-def find_small_key_diff_combinations(largest_diff_dict, threshold=25,ranges):
+def find_small_key_diff_combinations(largest_diff_dict, ranges,threshold=25):
     # 获取字典中的所有键
     keys = list(largest_diff_dict.keys())
 
@@ -17,12 +17,34 @@ def find_small_key_diff_combinations(largest_diff_dict, threshold=25,ranges):
     for (key1, key2) in itertools.combinations(keys, 2):
         # 计算键的差值
         diff = abs(key1 - key2)
-        
+        average_key_o = 0
+        if diff>180:
+            average_key = round((key1+key2-360)/2)
+            if average_key<0:
+                average_key = average_key +360
+                
+        else:
+            average_key = round((key1+key1)/2)
+        average_key_o = average_key
+        if average_key>180:
+            average_key = average_key-360
         # 如果差值小于给定的阈值，添加到 valid_combinations 中
-        if diff < threshold:
-            valid_combinations.append((key1, key2, diff))
+        # if diff < threshold:
+        valid_combinations.append((average_key, diff,average_key_o))
+    distance = 100
+    index_modi = 0
+    diff = 0
+    index = 0
+    for i in valid_combinations:
+        a,b ,c= i
+        if ranges[c]<distance:
+            distance = ranges[c]
+            index_modi = a
+            diff = b
+            index = c
 
-    return valid_combinations
+    goal = [index_modi,diff, index]
+    return goal
 
 def process_ranges(ranges):
     # 标记异常值：将 ranges 中为 0 的值标记为异常
@@ -38,23 +60,28 @@ def process_ranges(ranges):
     # 按照差分值排序字典（从大到小）
     sorted_diff_dict = {k: v for k, v in sorted(diff_dict.items(), key=lambda item: item[1], reverse=True)}
 
-    # 获取最大的两个差分值及其索引
-    threshold = 1  # 要找的最大数个数    
-    # 只返回包含最大差分值的字典
-    filtered_diff_dict = {k: v for k, v in sorted_diff_dict.items() if v > threshold}
+    # # 获取最大的两个差分值及其索引
+    # threshold = 0.5  # 要找的最大数个数    
+    # # 只返回包含最大差分值的字典
+    # filtered_diff_dict = {k: v for k, v in sorted_diff_dict.items() if v > threshold}
+
+    # 获取最大的前5个差分值及其索引
+    top_n = 5  # 选择前5个最大的差分值
+
+    filtered_diff_dict = {k: sorted_diff_dict[k] for k in list(sorted_diff_dict.keys())[:top_n]}
+
 
     largest_indices_sorted = list(filtered_diff_dict.keys())
 
     print(f"最大差分值的索引: {largest_indices_sorted}")
     print(f"最大差分值: {list(filtered_diff_dict.values())}")
     list1 = []
-    list1 = find_small_key_diff_combinations(filtered_diff_dict,25,ranges)
-    print(list1)
+    list1 = find_small_key_diff_combinations(filtered_diff_dict,ranges,15)
+    print("最终的目标，调整索引、夹角、原始索引",list1)
     return list1
 
-def publish_vel(list,ranges):
+def publish_vel(list1,ranges):
     global pub
-    a,b,c = list[0]
     # total_diff_sum = sum(diff_dict.values())
     # average_value = total_diff_sum/len(diff_dict)
     # 对字典的键求和
@@ -62,38 +89,23 @@ def publish_vel(list,ranges):
     # sorted_keys = list(diff_dict.keys())
     
     # 取出索引1和2的键值对
-    key1 = a
-    key2 = b
     
-    # 计算它们的差值
-    value_diff = abs(key1 - key2)
-    if value_diff>180:
-        average_key = round((key1+key2-360)/2)
-        if average_key<0:
-            average_key = average_key +360
-    else:
-        average_key = round((key1+key1)/2)
+    distance = ranges[list1[2]]
 
-    if average_key>180:
-        average_key = average_key-360
-
-
-    print("average_key",average_key)
-    distance = ranges[average_key]
-
-    print("distance",distance)
+    print("目标距离",distance)
     kw = 0.2
     kl = 0.5
     twist = Twist()
 
-    if c < 25:
+    print("夹角",list1[1])
+    if list1[1] < 15:
         twist.linear.x =  kl*distance # X 轴的线速度
-        twist.angular.z = kw*average_key  # 绕 Z 轴的角速度
-        # pub.publish(twist)
+        twist.angular.z = kw*list1[0]  # 绕 Z 轴的角速度
+        pub.publish(twist)
     else:
         twist.linear.x =  0# X 轴的线速度
         twist.angular.z = 0  # 绕 Z 轴的角速度
-        # pub.publish(twist)
+        pub.publish(twist)
     
 
 
